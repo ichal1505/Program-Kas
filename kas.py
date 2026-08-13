134590aead47f9411c2f8e7fb6efd6d883d3453e
ichal1505 <ichalm150599@gmail.com>
2026-08-13 10:56:42 +0800

    Update user credentials in kas.py

--- kas.py
+++ kas.py
@@ -41,8 +41,8 @@ st.markdown("""
 
 # --- Daftar akun yang bisa login ---
 USERS = {
-    "ichal": "kasku123",
-    "admin": "admin123",
+    "ichal": "150599",
+    "riska": "100198",
 }
 
 BULAN_LIST = [
